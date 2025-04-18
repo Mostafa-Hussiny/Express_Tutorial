8e549615168bf55a111707658e71bb7610cb3cd8
Mostafa <hussinym2@gmail.com>
2025-04-18 02:09:19 +0200

    Put & delete methods were added

--- Code/methods.js
+++ Code/methods.js
@@ -32,7 +32,7 @@ app.post('/api/postman/people', (req, res) =>{
     if (!name) {
         return res.status(400).json({success:false,msg:'please provide name value'})
     }
-    res.status(201).json({success: true,data:[...people]});
+    res.status(201).json({success: true,data:[...people, name]});
 })
 
 // in case the form is sending data itself not the js code in client side
@@ -44,6 +44,42 @@ app.post('/login', (req, res) => {
     }
 
     res.status(401).send('Please provide credentials')
+})
+
+
+//put method
+app.put('/api/people/:id', (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person) => person.id ===Number(id))
+
+    if (!person) {
+        return res.status(404).json({success: false, msg:`no person with id ${id}`})
+    }
+
+    const newPeople = people.map((person)=> {
+        if (person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+
+    res.status(200).json({success: true, data: newPeople})
+})
+
+// delete method
+app.delete('/api/people/:id', (req, res) => {
+    const {id} = req.params
+
+    const person = people.find((person) => person.id ===Number(id))
+
+    if (!person) {
+        return res.status(404).json({success: false, msg:`no person with id ${id}`})
+    }
+
+    const newPeople =people.filter((person) => person.id !== Number(id))
+    return res.status(200).json({success: true, data: newPeople})
 })
 
 app.listen(5000, () => {
